a0d644e7f3235744dca97d317addfa3d5cf89c3d
PhatDV <29118047+kingeni@users.noreply.github.com>
2018-07-10 19:00:41 +0700

    Basic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      works: []
+    };
+  }
+
+  onSubmit = () => {
+
+    var b = {
+      title: this.refs.title.value,
+      work: this.refs.content.value
+    };
+  let newWord = this.state.works;
+  newWord.push(b);
+    console.log(typeof newWord);
+    this.setState({ works: newWord });
+  }
+
   render() {
+    let element = this.state.works.map((work) => {
+      return <div>
+        <table>
+            <tr>
+                <td>{work.title}</td>
+                <td>{work.work}</td>
+            </tr>
+            </table>
+        </div>
+
+    });
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <td>Title: </td>
+              <td> <input type="text" ref="title" /> </td>
+            </tr>
+            <tr>
+              <td>Work: </td>
+              <td> <input type="text" ref="content" /> </td>
+            </tr>
+
+          </tbody>
+
+
+        </table>
+        <button type="submit" onClick={this.onSubmit} > Oke </button>
+       
+          {element}
+        
       </div>
     );
   }
